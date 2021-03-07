91cb2f15f740f0f2e6f3d84fba0410aed1c76314
Marco Maida <mmaida@mpi-sws.org>
2021-03-07 21:00:07 +0100

    first commit

---
+++ svgtest.py
@@ -0,0 +1,94 @@
+import svgwrite
+import datetime
+from datetime import date
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF
+
+##########################################
+# Helpers
+##########################################
+
+def cell(dwg, x, y, size, is_gone, is_birthday):
+    fill = 'black' if is_gone else 'white'
+    if is_birthday: fill = 'red'
+    dwg.add(dwg.circle(center=(x,y),
+        r=size, 
+        stroke=svgwrite.rgb(15, 15, 15, '%'),
+        fill=fill)
+    )
+
+def title (dwg, x, y, text):
+    dwg.add(dwg.text(text, insert=(x, y), font_size='100pt', fill='black', text_anchor='middle'))
+
+def text (dwg, x, y, text):
+    dwg.add(dwg.text(text, insert=(x, y), fill='black'))
+
+def line_header (dwg, x, y, text):
+    dwg.add(dwg.text(text, insert=(x, y), fill='black', text_anchor='end'))
+
+def setup_canvas (filename, width, height):
+    dwg = svgwrite.Drawing('test.svg', profile='tiny', size=(width,height))
+    dwg.add(dwg.rect((0,0), (width, height),fill='white'))
+
+    return dwg
+
+def export (dwg, svg_name, pdf_name):
+    dwg.save()
+
+    drawing = svg2rlg("test.svg")
+    renderPDF.drawToFile(drawing, "test.pdf")
+
+##########################################
+# Parameters
+##########################################
+
+today = datetime.date.today()
+birthday = datetime.date(1994, 8, 13)
+
+svg_name = 'test.svg'
+out_name = 'text.pdf'
+canvasx = 2100
+canvasy = 2970
+size = 10
+nx = 52
+ny = 90
+line_header_distance = 30
+marginleft = 200
+marginright = 100
+marginup = 250
+marginbottom = 100
+distancex = (canvasx - marginleft - marginright) / nx
+distancey = (canvasy - marginup - marginbottom) / ny
+
+##########################################
+# Drawing
+##########################################
+
+dwg = setup_canvas(svg_name, canvasx, canvasy)
+
+startx = marginleft + size/2
+starty = marginup + size/2
+date = birthday
+week = datetime.timedelta(days=7)
+for y in range(ny):
+    y = starty + y * distancey
+    line_header(dwg, startx - line_header_distance, y+ size/2, str(date.year))
+    for x in range(nx):
+        is_gone = date + week < today
+
+        this_year_birthday = datetime.date(date.year, birthday.month, birthday.day)
+        is_birthday = date <= this_year_birthday and this_year_birthday <= date + week
+
+        cell (dwg, startx + x * distancex, y, size,is_gone, is_birthday)
+
+        date += week
+
+
+title (dwg, canvasx/2, 150, "MY LIFE IN WEEKS")
+export(dwg, svg_name, out_name)
+
+
+"""
+dwg.add(dwg.line((20, 20), (2000, 2000), stroke=svgwrite.rgb(10, 10, 16, '%')))
+dwg.add(dwg.line((10, 10), (2000, 2000), stroke=svgwrite.rgb(10, 10, 16, '%')))
+"""
